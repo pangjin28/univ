5084001fce2d15c0fc42073e8f0ec8741da56553
pangjin28 <152086501+pangjin28@users.noreply.github.com>
2025-07-01 17:25:57 +0900

    Title: 비교 연산, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 250701/비교 연산/comparison-operator.py
+++ 250701/비교 연산/comparison-operator.py
@@ -1,19 +1,7 @@
 a,b=map(int,input().split())
-if a >=b:
-    print(1)
-else: print(0)
-if a >b:
-    print(1)
-else: print(0)
-if a <=b:
-    print(1)
-else: print(0)
-if a <b:
-    print(1)
-else: print(0)
-if a ==b:
-    print(1)
-else: print(0)
-if a !=b:
-    print(1)
-else: print(0)
+print(int(a >= b))
+print(int(a > b))
+print(int(a <= b))
+print(int(a < b))
+print(int(a == b))
+print(int(a != b))
