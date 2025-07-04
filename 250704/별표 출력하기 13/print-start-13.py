abe54771a1ef18205c4d81a4f743784cdf09356b
pangjin28 <152086501+pangjin28@users.noreply.github.com>
2025-07-04 16:53:57 +0900

    Title: 별표 출력하기 13, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250704/별표 출력하기 13/print-start-13.py
+++ 250704/별표 출력하기 13/print-start-13.py
@@ -1,15 +1,8 @@
 n = int(input())
 
-# 첫 줄
-print('* ' * n)
-
-# 중간 위쪽 (1부터 n-1까지 증가)
-print('*')
-for i in range(n - 1, 1, -1):
-    print('* ' * i)
-for i in range(2, n):
-    print('* ' * i)
-print('*')
-
-# 마지막 줄
-print('* ' * n)
+for i in range(2 * n):
+    if i % 2 == 0:  # 홀수 번째 줄 (0-based index) → i=0,2,4,...
+        stars = n - (i // 2)
+    else:           # 짝수 번째 줄
+        stars = (i + 1) // 2
+    print('* ' * stars)
