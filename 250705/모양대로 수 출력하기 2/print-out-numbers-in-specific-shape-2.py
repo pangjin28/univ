368b87cef1c4ea2894659a812c8cf9094133510c
pangjin28 <152086501+pangjin28@users.noreply.github.com>
2025-07-05 23:35:33 +0900

    Title: 모양대로 수 출력하기 2, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250705/모양대로 수 출력하기 2/print-out-numbers-in-specific-shape-2.py
+++ 250705/모양대로 수 출력하기 2/print-out-numbers-in-specific-shape-2.py
@@ -1,8 +1,10 @@
-N = int(input())
-
-for i in range(N):            # i: 행 인덱스
-    for j in range(N):        # j: 열 인덱스
-        # 2, 4, 6, 8 → 인덱스 0~3 순환 → ((i+j) % 4) → 값으로 바꾸려면 *2 + 2
-        value = ((i + j) % 4) * 2 + 2
-        print(value, end=' ')
-    print()
+n = int(input())
+cnt = 2
+	
+for _ in range(n):
+	for _ in range(n):
+		print(cnt, end=" ")
+		cnt += 2
+		if cnt == 10:
+			cnt = 2
+	print()
