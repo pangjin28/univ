f2bc4b4f217075edfdb9a1938dbddc03e642d2d8
pangjin28 <152086501+pangjin28@users.noreply.github.com>
2025-07-09 16:08:20 +0900

    Title: 나눗셈의 나머지, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250709/나눗셈의 나머지/remainder-of-division.py
+++ 250709/나눗셈의 나머지/remainder-of-division.py
@@ -2,7 +2,7 @@ A, B = map(int, input().split())
 counts = [0] * B
 
 while A > 1:  
-    A, r = divmod(A, B)
-    counts[r] += 1
+    counts[A % B] += 1
+    A //= B
 
 print(sum(x ** 2 for x in counts))
