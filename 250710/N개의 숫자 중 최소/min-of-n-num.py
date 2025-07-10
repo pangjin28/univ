27cf2c2cb69a32c6411de5182be20cc9d42ba925
pangjin28 <152086501+pangjin28@users.noreply.github.com>
2025-07-10 13:42:08 +0900

    Title: N개의 숫자 중 최소, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250710/N개의 숫자 중 최소/min-of-n-num.py
+++ 250710/N개의 숫자 중 최소/min-of-n-num.py
@@ -2,11 +2,12 @@ n = int(input())
 a = list(map(int, input().split()))
 
 # Please write your code here.
-mi = 100
+mi = float('inf')
 cnt=0
 for i in a:
     if i < mi:
         mi = i
-        if i == mi:
-            cnt+=1
+        cnt = 1
+    elif i == mi:
+        cnt+=1
 print(mi, cnt)
