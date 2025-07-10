3c4f450ae74ed97e6c301379dee5ede4942f4ad7
pangjin28 <152086501+pangjin28@users.noreply.github.com>
2025-07-10 13:51:24 +0900

    Title: 999 또는 -999, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250710/999 또는 -999/999-or-999.py
@@ -0,0 +1,11 @@
+a= list(map(int,input().split()))
+mi=a[0]
+ma=a[0]
+for i in a:
+    if i == 999 or i== -999:
+        break
+    if i < mi:
+        mi = i
+    if i > mi:
+        ma = i
+print(ma,mi)
