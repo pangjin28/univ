18a748938acf6d5be53ff2367b25f4b5a778380e
pangjin28 <152086501+pangjin28@users.noreply.github.com>
2025-07-11 19:00:34 +0900

    Title: 문자열 길이의 합, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250711/문자열 길이의 합/sum-length-of-string.py
+++ 250711/문자열 길이의 합/sum-length-of-string.py
@@ -1,8 +1,8 @@
 n= int(input())
 word =[input() for _ in range(n)]
-
-leng = len(word[0]) + len(word[1]) + len(word[2])
-
+leng=0
+for i in range(n):
+    leng += len(word[i])
 cnt =0
 for i in word:
     if i[0] =="a":
