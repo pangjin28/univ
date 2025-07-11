d4b524d9a8120713fe38042dc5c9393e668b276f
pangjin28 <152086501+pangjin28@users.noreply.github.com>
2025-07-11 17:47:53 +0900

    Title: 가장 짧은 문자열, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250711/가장 짧은 문자열/shortest-string.py
+++ 250711/가장 짧은 문자열/shortest-string.py
@@ -1,10 +1,8 @@
-a=input()
-b=input()
-c=input()
+a = input()
+b = input()
+c = input()
 
-if len(a) > len(b) and len(b) > len(c):
-    print(len(a)-len(c))
-elif len(b) > len(a) and len(a) > len(c):
-    print(len(b)-len(c))
-else:
-    print(len(c)- len(a))
+
+lengths = [len(a), len(b), len(c)]
+diff = max(lengths) - min(lengths)
+print(diff)
