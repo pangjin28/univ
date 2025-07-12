38b579083b9743adccb3e9ea2c14da73ca9b942d
pangjin28 <152086501+pangjin28@users.noreply.github.com>
2025-07-12 16:38:47 +0900

    Title: 특정 문자의 등장 횟수, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250712/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.py
@@ -0,0 +1,11 @@
+a= input()
+cnt1=0
+cnt2=0
+
+for i in range(len(a)-1):
+    if a[i] == "e" and a[i+1] =="e":
+        cnt1+=1
+    elif a[i] == "e" and a[i+1] =="b":
+        cnt2+=1
+
+print(cnt1,cnt2)
