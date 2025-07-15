c344825a7d3cb0c2a2d78ef88ab4f1161464d436
pangjin28 <152086501+pangjin28@users.noreply.github.com>
2025-07-15 22:47:31 +0900

    Title: 숫자로 이루어진 사각형, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250715/숫자로 이루어진 사각형/rectangle-with-a-number.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+# Please write your code here.
+def saa(a):
+    ds=[[0]*a for _ in range(a)]
+    cnt = 1
+    for i in range(a):
+        for j in range(a):
+            ds[i][j] = cnt
+            cnt+=1
+            if cnt > 9:
+                cnt = 1
+
+    for i in range(a):
+        for j in range(a):
+            print(ds[i][j],end=" ")
+        print()
+saa(4)
