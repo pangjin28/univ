d743af85d92744d4d3eb4e1e3759437954303eca
pangjin28 <152086501+pangjin28@users.noreply.github.com>
2025-07-17 21:33:21 +0900

    Title: 나누고 빼면서 합하기, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250717/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+A = list(map(int, input().split()))
+
+# Please write your code here.
+def sa(m):
+    sum =0
+    while True:
+        sum += A[m-1]
+        if m ==1:
+            break
+        if m % 2 ==0:
+            m //=2
+        else:
+            m-=1
+    return sum
+print(sa(m))
