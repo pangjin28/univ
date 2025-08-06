abfba875b07421cbc7ea7add6c8b9bff56c0a1c1
pangjin28 <152086501+pangjin28@users.noreply.github.com>
2025-08-06 22:26:41 +0900

    Title: 개발자의 능력 2, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250806/개발자의 능력 2/ability-of-developer-2.py
@@ -0,0 +1,26 @@
+n = 6
+arr = list(map(int, input().split()))
+
+
+def diff(i, j, k, l):
+	sum1 = arr[i] + arr[j]
+	sum2 = arr[k] + arr[l]
+	sum3 = sum(arr) - sum1 - sum2
+	
+	ret = abs(sum1 - sum2)
+	ret = max(ret, abs(sum2 - sum3))
+	ret = max(ret, abs(sum3 - sum1))
+	
+	return ret
+
+
+min_diff = float("inf")
+for i in range(n):
+	for j in range(i + 1, n):
+		for k in range(n):
+			for l in range(k + 1, n):
+				if k == i or k == j or l == i or l == j:
+					continue
+				min_diff = min(min_diff, diff(i, j, k, l))
+
+print(min_diff)
