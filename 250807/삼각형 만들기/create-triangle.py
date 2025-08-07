d999a2da9705546b792d2b0c88d7549f57131fa5
pangjin28 <152086501+pangjin28@users.noreply.github.com>
2025-08-07 16:36:51 +0900

    Title: 삼각형 만들기, Time: 52ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250807/삼각형 만들기/create-triangle.py
@@ -0,0 +1,14 @@
+n = int(input()) 
+points = [tuple(map(int, input().split())) for _ in range(n)]
+
+def area(p1, p2, p3):
+    return abs(p1[0]*(p2[1] - p3[1]) + p2[0]*(p3[1] - p1[1]) + p3[0]*(p1[1] - p2[1])) / 2
+
+max_area = 0
+
+for i in range(n):
+    for j in range(i + 1, n):
+        for k in range(j + 1, n):
+                max_area = max(max_area, area(points[i], points[j], points[k]))
+
+print(2 * int(max_area)) 
