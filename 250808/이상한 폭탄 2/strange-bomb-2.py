92d155eca6d5a6ebc31fa5b424a0b8782788f711
pangjin28 <152086501+pangjin28@users.noreply.github.com>
2025-08-08 16:27:15 +0900

    Title: 이상한 폭탄 2, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 250808/이상한 폭탄 2/strange-bomb-2.py
+++ 250808/이상한 폭탄 2/strange-bomb-2.py
@@ -1,14 +1,11 @@
 N, K = map(int, input().split())
 num = [int(input()) for _ in range(N)]
 
-# Please write your code here.
-maxd= 0
+max_bomb = -1
+
 for i in range(N):
     for j in range(i+1, N):
-        if num[i] == num[j]:
-            if j - i <= K:
-                a = num[j]
-            maxd = max(maxd, a)
-
-print(maxd)
-            
+        if num[i] == num[j] and j - i <= K:
+            if num[i] > max_bomb:
+                max_bomb = num[i]
+print(max_bomb)
