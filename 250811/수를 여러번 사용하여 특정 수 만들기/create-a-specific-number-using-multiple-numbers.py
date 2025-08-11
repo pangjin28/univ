e0da672f3224dac44688f6922ae37e579056cb19
pangjin28 <152086501+pangjin28@users.noreply.github.com>
2025-08-11 17:46:42 +0900

    Title: 수를 여러번 사용하여 특정 수 만들기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250811/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.py
+++ 250811/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.py
@@ -2,11 +2,9 @@ A, B, C = map(int, input().split())
 
 # Please write your code here.
 maxd =0
-for i in range(1000):
-    for j in range(1000):
+for i in range(C // A + 1):
+    for j in range(C // B + 1):
         s = A * i + B * j
-        if s > C:
-            break
         if s <= C:
             maxd= max(maxd, s)
 
