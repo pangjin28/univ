8d2544183d0a3601719277cfe81c5c4e8249bf66
pangjin28 <152086501+pangjin28@users.noreply.github.com>
2025-08-11 17:18:54 +0900

    Title: 야바위, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250811/야바위/ya-rock.py
@@ -0,0 +1,23 @@
+n = int(input())
+moves = [tuple(map(int, input().split())) for _ in range(n)]
+
+# Please write your code here.
+best_pos = 0
+max_score = 0
+
+for start in range(1, 4):
+    pos = start
+    score = 0
+    for a, b, c in moves:
+        if pos == a:
+            pos = b
+        elif pos == b:
+            pos = a
+
+        if pos == c:
+            score += 1
+    if score > max_score:
+        max_score = score
+        best_pos = start
+
+print(best_pos)
